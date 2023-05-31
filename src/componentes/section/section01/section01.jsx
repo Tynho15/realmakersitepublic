66acf4adfabfc094e0f5aecb6c0fc3c853984879
JOSE COSTA TOBIAS <125271@pmenos.com.br>
2023-05-31 17:01:55 -0300

    versão 0.1

--- src/componentes/section/section01/section01.jsx
+++ src/componentes/section/section01/section01.jsx
@@ -1,13 +1,13 @@
 
-
+import './style.css';
 
 function Section01(){
     return(
-        <section>
-               <p>Real Maker Studio
+        <section className='section01'>
+           <h2 className="tituloReal"> Real Maker Studio </h2>
+               <p className='suberTitulo'>
                 Startup desenvolvedora de jogos indie
                 e independentes Mobile e PC
-
             </p>
         </section>
     )
