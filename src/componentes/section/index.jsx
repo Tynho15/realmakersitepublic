214b87a3b86d73000f25397c5bee6a6f7ac25928
JOSE COSTA TOBIAS <125271@pmenos.com.br>
2023-06-06 14:51:40 -0300

    versão 00.02

--- src/componentes/section/index.jsx
+++ src/componentes/section/index.jsx
@@ -17,6 +17,10 @@ function Section(){
             <p>Startup desenvolvedora de jogos indie para dispositivos móveis e PC.</p>
             <img src={logo} alt="logo" />
         </div>
+        <iframe width="560" height="315" src="https://www.youtube.com/watch?v=pKulmkhRRY0"
+                    title="YouTube video player" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    allowfullscreen></iframe>
     </section>
 </div>
 
