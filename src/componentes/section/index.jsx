8f6bc788524821c71b0498cc3aa37f6519cd0576
JOSE COSTA TOBIAS <125271@pmenos.com.br>
2023-06-09 19:18:22 -0300

    00

--- src/componentes/section/index.jsx
+++ src/componentes/section/index.jsx
@@ -8,14 +8,14 @@ function Section() {
         <div className="paiSection">
 
             <section id="inicio" className="section01">
-                <h2 className='tituloSection01'>Sobre</h2>
+              
+            </section>
+            <section id="jogos" className="section02">
+            <h2 className='tituloSection01'>Sobre</h2>
                 <p className='textSection01'>
                     Realmakerstudio é um Studio indie de jogos de pixelart criada por João Vitor Nunes Oliveira, um jovem empreendedor de 21 anos. Com uma paixão ardente pela arte dos pixels, a Realmakerstudio cria jogos encantadores que trazem à vida mundos retro com uma estética única. Sob a liderança de João Vitor, a equipe talentosa da empresa se dedica a criar experiências envolventes e cativantes para os jogadores. Com sua abordagem simples e charmosa, os jogos da Realmakerstudio são uma celebração da nostalgia e da criatividade, proporcionando momentos de diversão e emoção para os fãs de jogos indie.
 
                 </p>
-            </section>
-            <section id="jogos" className="section02">
-                {/*  <!-- Conteúdo da seção 02 --> */}
             </section>
             <section id="servicos" className="section03">
                 <div className="content">
