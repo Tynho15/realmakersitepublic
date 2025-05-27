bc9bdc6a1c0a8afa804f10e79e7e319bcbf216a7
jockaplay <jocksonmatheus@gmail.com>
2025-05-27 01:51:14 -0300

    feat: replace YouTube iframe with local video for improved performance; add trailer video asset

--- src/Sections/Landing/index.tsx
+++ src/Sections/Landing/index.tsx
@@ -1,17 +1,14 @@
 import { Button } from '@/components/ui/button';
 import { scrollToId } from '@/utils/goToAction';
+import video from '../../assets/videos/trailer.mp4';
 
 const Landing = () => {
   const doc  = document
   return(
     <div className="flex relative flex-col items-center justify-center h-screen bg-neutral-950">
       <div className="absolute inset-0 bg-gradient-to-b from-transparent via-black/40 to-black/90 z-1"/>
-      <iframe
-        className="absolute opacity-50 inset-0 w-full h-full object-cover pointer-events-none"
-        src="https://www.youtube.com/embed/NILtTocqZ84?autoplay=1&mute=1&loop=1&playlist=NILtTocqZ84&controls=0&showinfo=0&modestbranding=1"
-        allow="autoplay; encrypted-media"
-        aria-controls='false'
-      ></iframe>
+      <video className="absolute opacity-50 inset-0 w-full h-full object-cover" autoPlay loop muted playsInline src={video}>
+      </video>
       <h1 className="text-4xl z-1 font-bold mb-4 text-gray-200 items-center text-center">Bem vindo à <span className="text-red-600">Real Maker Studio</span>!</h1>
       <p className="text-lg z-1 text-gray-400 mb-8 text-center">Startup desenvolvedora de jogos indie para dispositivos móveis e PC.</p>
       <Button onClick={() => scrollToId({document: doc, dest: "ultimo-jogo"})} className='z-1 cursor-pointer'>
